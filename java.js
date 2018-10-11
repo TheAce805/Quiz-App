f24a3af7892c11875e8c2576a0919fe1f5eb1cd1
Diego Buenrostro <42652866+TheAce805@users.noreply.github.com>
2018-10-11 10:20:45 -0700

    Create java.js

---
+++ java.js
@@ -0,0 +1,23 @@
+$("document").ready(function(){
+  $("#b").click(function(){
+    $(".hide").toggle();
+  })
+})
+
+$("document").ready(function(){
+$("#a").click(function(){
+    $(".boo").toggle();
+  })
+})
+
+  $("document").ready(function(){
+    $("#c").click(function(){
+      $(".gone").toggle();
+    })
+  })
+
+  $("document").ready(function(){
+    $("#d").click(function(){
+      $(".bye").toggle();
+    })
+  })

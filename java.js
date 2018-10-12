78c6cae11dd4b5ac8d19f5a6a19a60fe72a81a80
Diego Buenrostro <42652866+TheAce805@users.noreply.github.com>
2018-10-12 10:17:35 -0700

    Update java.js

--- java.js
+++ java.js
@@ -1,23 +1,101 @@
-$("document").ready(function(){
-  $("#b").click(function(){
-    $(".hide").toggle();
-  })
-})
+$(document).ready(function(){
+	$("#A2").click(function(){
+	 $("#A2").text("Correct!");
+	} )
+ } ) 
+ 
+  $(document).ready(function(){
+	$("#A5").click(function(){
+	 $("#A5").text("Correct!");
+	} )
+ } ) 
+ 
+ $(document).ready(function(){
+	$("#A2").click(function(){
+	 $("#A2").text("Correct!");
+	} )
+ } ) 
+ 
+ $(document).ready(function(){
+	$("#A11").click(function(){
+	 $("#A11").text("Correct!");
+	} )
+ } ) 
+ 
+ $(document).ready(function(){
+	$("#A16").click(function(){
+	 $("#A16").text("Correct!");
+	} )
+ } ) 
+ 
+ $(document).ready(function(){
+	$("#A1").click(function(){
+	 $("#A1").text("Incorrect!");
+	} )
+ } ) 
+ 
+  $(document).ready(function(){
+	$("#A3").click(function(){
+	 $("#A3").text("Incorrect!");
+	} )
+ } ) 
+ 
+  $(document).ready(function(){
+	$("#A4").click(function(){
+	 $("#A4").text("Incorrect!");
+	} )
+ } ) 
+ 
+  $(document).ready(function(){
+	$("#A6").click(function(){
+	 $("#A6").text("Incorrect!");
+	} )
+ } ) 
+ 
+  $(document).ready(function(){
+	$("#A7").click(function(){
+	 $("#A7").text("Incorrect!");
+	} )
+ } ) 
+ 
+  $(document).ready(function(){
+	$("#A8").click(function(){
+	 $("#A8").text("Incorrect!");
+	} )
+ } ) 
+ 
+  $(document).ready(function(){
+	$("#A9").click(function(){
+	 $("#A9").text("Incorrect!");
+	} )
+ } )
 
-$("document").ready(function(){
-$("#a").click(function(){
-    $(".boo").toggle();
-  })
-})
+ $(document).ready(function(){
+	$("#A10").click(function(){
+	 $("#A10").text("Incorrect!");
+	} )
+ } )
 
-  $("document").ready(function(){
-    $("#c").click(function(){
-      $(".gone").toggle();
-    })
-  })
+ $(document).ready(function(){
+	$("#A12").click(function(){
+	 $("#A12").text("Incorrect!");
+	} )
+ } )
 
-  $("document").ready(function(){
-    $("#d").click(function(){
-      $(".bye").toggle();
-    })
-  })
+ $(document).ready(function(){
+	$("#A13").click(function(){
+	 $("#A13").text("Incorrect!");
+	} )
+ } )
+
+ $(document).ready(function(){
+	$("#A14").click(function(){
+	 $("#A14").text("Incorrect!");
+	} )
+ } )
+
+ $(document).ready(function(){
+	$("#A15").click(function(){
+	 $("#A15").text("Incorrect!");
+	} )
+ } )  
